983b0bb405c64169caab25f4b593e9a31a529f60
mtokarev <mtokarev@wavesplatform.com>
2020-07-16 19:05:10 +0300

    temporary hack, upd package version

--- src/api/node/transactions.x.ts
+++ src/api/node/transactions.x.ts
@@ -749,7 +749,7 @@ export const postDockerCreateV2 = d => {
 
   const result = {
     ...d,
-    feeAssetId: normalizeAssetId(d.feeAssetId),
+    feeAssetId: null,
     params: data,
     transactionType: null,
     type: constants.DOCKER_CREATE_TX_V2,
